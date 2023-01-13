4f17a119fef7a7278f37435bc6015999a2e8c8cb
Emmanuel <ejaramilo@hotmail.com>
2023-01-13 09:04:31 -0500

    13010904 eliminated trix styles to enable page load

--- config/routes.rb
+++ config/routes.rb
@@ -6,10 +6,9 @@ Rails.application.routes.draw do
   get 'articles/edit'
   get 'articles/update'
   get 'articles/destroy'
-  resources :articles
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "articles#index"
 end
